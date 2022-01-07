5cca2922755603e366474529983d2e5aa496fc9b
hanson97271 <92702155+hanson97271@users.noreply.github.com>
2022-01-07 15:27:57 +0800

    Add files via upload

---
+++ hw14_2.java
@@ -0,0 +1,19 @@
+package Java14;
+import java.util.Scanner;
+import java.lang.String;
+
+public class hw14_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str;
+		
+		System.out.print("¿é¤J¦r¦ê");
+		str=sc.nextLine();
+		
+		System.out.println("¤j¼g"+str.toUpperCase());
+
+	}
+
+}
